61febc55bd81ca6b771936d8813e201711cdecc9
ymk <1766273199@qq.com>
2023-07-26 00:18:22 +0800

    开始实现主流程

--- main.go
+++ main.go
@@ -1,4 +1,36 @@
 package main
 
+type CmdType = byte
+
+type GodisDB struct {
+	data   *Dict
+	expire *Dict
+}
+
+type GodisServer struct {
+	fd      int
+	port    int
+	db      *GodisDB
+	clients map[int]*GodisClient
+	keLoop  *KeLoop
+}
+
+type GodisClient struct {
+	fd       int
+	db       *GodisDB
+	args     []*Gobj
+	reply    *List
+	sentLen  int
+	queryBuf []byte
+	queryLen int
+	cmdType  CmdType
+	bulkNum  int
+	bulkLen  int
+}
+
+func ReadQueryFromClient(loop *KeLoop, fd int, extra interface{}) {
+
+}
+
 func main() {
 }
