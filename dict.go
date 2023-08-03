9068afa39ccf8c9ea13c7c4f91d0fcd09f31cd9f
ymk <1766273199@qq.com>
2023-08-04 01:54:50 +0800

    DICT实现中。

--- dict.go
+++ dict.go
@@ -1,5 +1,10 @@
 package main
 
+const (
+	DEFAULT_STEP int   = 1
+	INT_SIZE     int64 = 8
+)
+
 // 包含两个方法，求哈希和比较
 type DictType struct {
 	HashFunc  func(key *Gobj) int64
@@ -32,3 +37,31 @@ func DictCreate(dictType DictType) *Dict {
 	return &dict
 }
 
+// 是否在rehash
+func (dict *Dict) isRehashing() bool {
+	return dict.rehashidx != -1
+}
+
+func (dict *Dict) rehashStep() {
+	dict.rehash(DEFAULT_STEP)
+}
+
+func (dict *Dict) rehash(step int) {
+
+}
+
+func (dict *Dict) nextPower(size int64) int64 {
+
+}
+
+func (dict *Dict) expand(size int64) error {
+
+}
+
+func (dict *Dict) expandIfNeeded() error {
+
+}
+
+func (dict *Dict) keyIndex(key *Gobj) int64 {
+
+}
